4f7a71013f891b28ef76529640c774e598b2d8bb
Gregory Casamento <greg.casamento@gmail.com>
2023-08-12 08:27:11 -0400

    Initial Commit

---
+++ NSTableViewXib_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTableViewXib_test
+//
+//  Created by Gregory John Casamento on 8/12/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSTableViewXib_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSTableViewXib_test
+//
+//  Created by Gregory John Casamento on 8/12/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
